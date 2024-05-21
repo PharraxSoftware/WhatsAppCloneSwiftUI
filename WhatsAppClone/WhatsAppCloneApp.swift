62ca08134497ae53ee869be5c51e0409eb5c613d
macstudio <macos@example.com>
2024-05-21 06:10:44 +0300

    Initial Commit

---
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by YILMAZ ER on 21.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
